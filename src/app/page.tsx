fe458cd084e4771d989e6f9e3ea630a441703599
Surya Kiran Palipireddy <113004628+surya9490@users.noreply.github.com>
2024-10-02 20:17:42 +0530

    porfolio

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+
+import { Footer } from "@/sections/Footer";
+import { Header } from "@/sections/Header";
+import { HeroSection } from "@/sections/Hero";
+import { ProjectsSection } from "@/sections/Projects";
+
+export default function Home() {
+  return (
+    <div>
+      <Header />
+      <HeroSection />
+      <ProjectsSection />
+    </div>
+  );
+}
---
+++ src/components/HeroOrbit.tsx
@@ -0,0 +1,22 @@
+import { PropsWithChildren } from "react";
+
+
+export const HeroOrbit = ({
+  children,
+  size,
+  rotation
+}: PropsWithChildren<{ size: number,rotation:number }>) => {
+  return (
+    <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
+      <div
+        className="animate-spin [animation-duration:30s] flex items-start justify-start"
+      style={{ height:`${size}px`, width:`${size}px`, transform: `rotate(${rotation}deg)` }}
+      >
+        <div className="inline-flex animate-spin [animation-duration:5s]"
+        style={{ transform: `rotate(${rotation * -1}deg)`}}>
+          {children}
+        </div>
+      </div>
+    </div>
+  );
+};
